5da63bbc03981540240b3c6a8cd8dfa9b4765823
Meenakshi Mani <meena@formationds.com>
2016-10-12 14:25:48 -0600

    Removed debug statements

--- dependency_setup.sh
+++ dependency_setup.sh
@@ -3,10 +3,8 @@
 set -e
 
 wget http://bld-artifactory:8081/artifactory/formation-apt/pool/nightly/boost-1.55_1.55_amd64.deb
-sudo -s << EOF
-echo $USER
+sudo -s << 'EOF'
 dpkg -i boost-1.55_1.55_amd64.deb
 EOF
-echo $USER
 wget http://bld-artifactory:8081/artifactory/external_packages/curl-7.36.0.tar.gz
 tar xvzf curl-7.36.0.tar.gz
